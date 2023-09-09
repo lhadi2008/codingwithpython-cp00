413526c07c17c7c1d23f41f1f36b89d430e40a7b
lhadi2008 <139581771+lhadi2008@users.noreply.github.com>
2023-09-09 10:23:07 +0100

    Add files via upload

--- guess.py
+++ guess.py
@@ -9,14 +9,25 @@ print("I'm thinking of a number between 1 and 100. Can you guess what it is?")
 
 guess = randint(1,100)
 
-your_guess = int(input())
+user_input = int(input())
 
-if your_guess < guess:
-    print('your guess is too low, try again')
-elif your_guess > guess:
-    print('your guess is too high , try again')
-elif your_guess == guess:
-    print('you guessed it right, it s {guess}!')
+if user_input > 100:
+    print('no values superior than 100 are accepted')
+
+while user_input != guess:
+   if user_input < guess:
+      print('your guess is too low, try again')
+      user_input = int(input())
+   elif user_input > guess:
+      print('your guess is too high , try again')
+      user_input = int(input())
+   elif user_input == guess:
+      print('You guessed it correct')
+     
+
+
+
+
 
 
 
